fd10677a80ec0b7814ef5f8e3f8e0f3885e427fe
XiaMo <61896940+Xiamo-vip@users.noreply.github.com>
2023-01-27 22:34:12 +0800

    Update Theme.kt

--- app/src/main/java/com/xiamo/baodian/ui/theme/Theme.kt
+++ app/src/main/java/com/xiamo/baodian/ui/theme/Theme.kt
@@ -40,9 +40,9 @@ private val LightColorPalette = lightColors(
 @Composable
 fun BaodianTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        LightColorPalette
-    } else {
         DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
